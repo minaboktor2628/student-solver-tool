616b99d06d8ea65f22eee083bcdcf04c9a9540c7
Jacob Burns <jaburns@wpi.edu>
2025-09-22 10:25:36 -0400

    import course/meetings REs

--- src/lib/validation.ts
+++ src/lib/validation.ts
@@ -1,9 +1,11 @@
-import type {
-  Allocation,
-  AllocationWithoutAssistants,
-  Assignment,
-  Assistant,
-  AssistantPreferences,
+import {
+  COURSE_RE,
+  MEETING_RE,
+  type Allocation,
+  type AllocationWithoutAssistants,
+  type Assignment,
+  type Assistant,
+  type AssistantPreferences,
 } from "@/types/excel";
 
 export function mergeAllocationsAndAssignments(
@@ -32,13 +34,11 @@ export function mergeAllocationsAndAssignments(
 export function makeCourseToAssistantMap(data: AssistantPreferences[]) {
   const courseMap: Record<string, AssistantPreferences[]> = {};
 
-  const courseRegex = /^CS \d{3,4}$/;
-
   // if the key is like "CS 2023" and the value at that key is true,
   // add that assistant to the available list of assistants for that course
   for (const student of data) {
     for (const [key, value] of Object.entries(student)) {
-      if (courseRegex.test(key) && value === true) {
+      if (COURSE_RE.test(key) && value === true) {
         (courseMap[key] ??= []).push(student);
       }
     }
@@ -50,12 +50,9 @@ export function makeCourseToAssistantMap(data: AssistantPreferences[]) {
 export function makeMeetingToAssistantMap(data: AssistantPreferences[]) {
   const courseMap: Record<string, AssistantPreferences[]> = {};
 
-  const meeetingRegex =
-    /^(?<days>[MTWRF](?:-[MTWRF])*)\s*\|?\s*(?<start>\d{1,2}:\d{2}\s[AP]M)\s*-\s*(?<end>\d{1,2}:\d{2}\s[AP]M)$/;
-
   for (const student of data) {
     for (const [key, value] of Object.entries(student)) {
-      if (meeetingRegex.test(key) && value === true) {
+      if (MEETING_RE.test(key) && value === true) {
         (courseMap[key] ??= []).push(student);
       }
     }
