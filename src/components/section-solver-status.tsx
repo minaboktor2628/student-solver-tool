488f93d6ecb030df7fcae88fb5dbf90bb542e615
minaboktor2628 <mina26b8@gmail.com>
2025-11-12 15:52:11 -0500

    added badge that shows the number of assigned to number of required hours

---
+++ src/components/section-solver-status.tsx
@@ -0,0 +1,50 @@
+import { Badge } from "@/components/ui/badge";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export type SectionSolverStatusProps = {
+  marginOfError: number;
+  hoursRequired: number;
+  hoursAssigned: number;
+};
+
+export function SectionSolverStatus({
+  hoursRequired,
+  marginOfError,
+  hoursAssigned,
+}: SectionSolverStatusProps) {
+  const difference = Math.abs(hoursRequired - hoursAssigned);
+  const isAssignedExactlyNumberOfHoursNeeded = difference === 0;
+  const isWithinMarginOfError =
+    difference <= marginOfError && !isAssignedExactlyNumberOfHoursNeeded;
+
+  return (
+    <Tooltip>
+      <TooltipTrigger className="ml-auto">
+        <Badge
+          variant={
+            isAssignedExactlyNumberOfHoursNeeded
+              ? "success"
+              : isWithinMarginOfError
+                ? "warning"
+                : "destructive"
+          }
+        >
+          {hoursAssigned}/{hoursRequired}
+        </Badge>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>
+          {isAssignedExactlyNumberOfHoursNeeded
+            ? "This class's assigned assistant hours are exactly what the course needs."
+            : isWithinMarginOfError
+              ? "This class's assigned assistant hours are within an acceptable range."
+              : "This class's assigned assistant hours are either too high or too low."}
+        </p>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
