56e00fa47a80d3e66bf2a296390e4ce13eb602a3
minaboktor2628 <mina26b8@gmail.com>
2025-12-09 20:23:20 -0500

    add active field to seed command

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -29,6 +29,7 @@ async function main() {
       year: 2025,
       termStaffDueDate: new Date("2025-08-15T23:59:59Z"),
       termProfessorDueDate: new Date("2025-08-10T23:59:59Z"),
+      active: true,
     },
   });
 
