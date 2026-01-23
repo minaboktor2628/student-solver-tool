44ae865e078fb23bd456ff329a77d5767b20ed72
minaboktor2628 <mina26b8@gmail.com>
2026-01-23 18:34:01 -0500

    add prisma studio (no auth)

---
+++ src/app/api/studio/route.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import { createNodeSQLiteExecutor } from "@prisma/studio-core/data/node-sqlite";
+import { serializeError } from "@prisma/studio-core/data/bff";
+import DatabaseSync from "better-sqlite3";
+import { env } from "@/env";
+
+export const runtime = "nodejs";
+export const dynamic = "force-dynamic";
+
+export async function POST(req: NextRequest) {
+  try {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const body = await req.json();
+
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const { query } = body;
+
+    if (!query) {
+      return NextResponse.json(
+        [serializeError(new Error("Query is required"))],
+        { status: 400 },
+      );
+    }
+
+    const url = env.DATABASE_URL;
+    if (!url) {
+      return NextResponse.json(
+        [serializeError(new Error("DATABASE_URL is missing"))],
+        { status: 500 },
+      );
+    }
+
+    // FIX: for some reason, we have to replace it with prisma/ instead of ""
+    const dbPath = url.replace("file:", "prisma/");
+    const database = new DatabaseSync(dbPath);
+    const [error, results] =
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      await createNodeSQLiteExecutor(database).execute(query);
+
+    if (error) {
+      console.error(error);
+      return NextResponse.json([serializeError(error)]);
+    }
+
+    return NextResponse.json([null, results]);
+  } catch (err) {
+    console.error(err);
+    return NextResponse.json([serializeError(err as Error)], { status: 400 });
+  }
+}
