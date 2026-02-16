44b974c37083aae133893adc2f41006070904b52
Jacob Burns <jaburns@wpi.edu>
2026-02-16 08:49:07 -0500

    staff dashboard

--- src/components/staff/staff-homepage.tsx
+++ src/components/staff/staff-homepage.tsx
@@ -4,6 +4,8 @@ import { DeadlineCard } from "@/components/staff/staff-deadline-card";
 import { useTerm } from "@/components/term-combobox";
 import { Header } from "@/components/staff/staff-dashboard-header";
 import { api } from "@/trpc/react";
+import StaffDashboardFormSumary from "./staff-dashboard-form-summary";
+import StaffAssignment from "./staff-assignment-card";
 
 interface StaffHomePageProps {
   userId: string;
@@ -12,13 +14,7 @@ const StaffHomePage: React.FC<StaffHomePageProps> = ({ userId }) => {
   const { active: activeTerm } = useTerm();
   if (!activeTerm) throw new Error("Term is invalid.");
 
-  const pastAssignmentSections =
-    api.studentDashboard.getPastAssignments.useSuspenseQuery({
-      userId: userId,
-      termId: activeTerm.id,
-    });
-
-  const currentAssignment =
+  const [currentAssignment] =
     api.studentDashboard.getCurrentAssignment.useSuspenseQuery({
       userId: userId,
       termId: activeTerm.id,
@@ -36,6 +32,10 @@ const StaffHomePage: React.FC<StaffHomePageProps> = ({ userId }) => {
       termId: activeTerm.id,
     });
 
+  const [{ canEdit }] = api.studentForm.getCanEdit.useSuspenseQuery({
+    userId: userId ?? "",
+  });
+
   const username = info.name;
   const deadlineDate = info.term.dueDate;
   if (!deadlineDate) throw new Error("Deadline Date Invalid");
@@ -43,9 +43,32 @@ const StaffHomePage: React.FC<StaffHomePageProps> = ({ userId }) => {
   return (
     <div className="container mx-auto max-w-6xl p-6">
       <Header username={username} />
-      <DeadlineCard deadlineDate={deadlineDate} isSubmitted={hasSubmitted} />
-      {/* Courses Overview */}
-      {/* <CoursesCard sections={professorSections} isSubmitted={isSubmitted} /> */}
+      {canEdit?.canEditForm && (
+        <div>
+          <DeadlineCard
+            deadlineDate={deadlineDate}
+            isSubmitted={hasSubmitted}
+          />
+          <StaffDashboardFormSumary userId={userId} termId={activeTerm.id} />
+        </div>
+      )}
+      {currentAssignment?.assignment && (
+        <StaffAssignment
+          termLetter={activeTerm.termLetter}
+          year={activeTerm.year}
+          courseTitle={currentAssignment.assignment.section.courseTitle}
+          courseCode={currentAssignment.assignment.section.courseCode}
+          courseSection={currentAssignment.assignment.section.courseSection}
+          meetingPattern={currentAssignment.assignment.section.meetingPattern}
+          professorName={currentAssignment.assignment.section.professor.name}
+          professorEmail={currentAssignment.assignment.section.professor.email}
+        />
+      )}
+      {!canEdit?.canEditForm && !currentAssignment?.assignment && (
+        <h2>
+          Assignments currently in progress, check back soon for your assignment
+        </h2>
+      )}
     </div>
   );
 };
--- src/server/api/routers/student-dashboard.ts
+++ src/server/api/routers/student-dashboard.ts
@@ -1,14 +1,58 @@
 import { z } from "zod";
 import { assistantProcedure, createTRPCRouter } from "../trpc";
-import { TRPCError } from "@trpc/server";
-import { hasPermission } from "@/lib/permissions";
-import { TermProvider } from "@/components/term-combobox";
 
 const baseInput = z.object({
   userId: z.string().min(1),
   termId: z.string().min(1),
 });
 export const studentDashboardRoute = createTRPCRouter({
+  getPreferencesFormData: assistantProcedure
+    .input(baseInput)
+    .query(async ({ input: { userId, termId }, ctx }) => {
+      const staffPref = await ctx.db.staffPreference.findUnique({
+        where: { userId_termId: { userId, termId } },
+        include: {
+          timesAvailable: true,
+          qualifiedForSections: {
+            include: {
+              section: true,
+            },
+          },
+          preferredSections: {
+            include: {
+              section: true,
+            },
+          },
+        },
+      });
+
+      if (!staffPref) {
+        return null;
+      }
+
+      return {
+        isAvailableForTerm: staffPref.isAvailableForTerm,
+        timesAvailable: staffPref.timesAvailable.map((t) => ({
+          day: t.day,
+          hour: t.hour,
+        })),
+        qualifications: staffPref.qualifiedForSections.map((q) => ({
+          sectionId: q.sectionId,
+          courseCode: q.section.courseCode,
+          courseSection: q.section.courseSection,
+          courseTitle: q.section.courseTitle,
+        })),
+        preferences: staffPref.preferredSections.map((p) => ({
+          sectionId: p.sectionId,
+          courseCode: p.section.courseCode,
+          courseSection: p.section.courseSection,
+          courseTitle: p.section.courseTitle,
+          rank: p.rank,
+        })),
+        comments: staffPref.comments,
+      };
+    }),
+
   hasSubmittedPreferencesForm: assistantProcedure
     .input(baseInput)
     .query(async ({ input: { userId, termId }, ctx }) => {
@@ -67,20 +111,31 @@ export const studentDashboardRoute = createTRPCRouter({
     }),
 
   getPastAssignments: assistantProcedure
-    .input(baseInput)
-    .query(async ({ input: { userId, termId }, ctx }) => {
+    .input(z.object({ userId: z.string() }))
+    .query(async ({ input: { userId }, ctx }) => {
       //TODO add auth check
 
       const rows = await ctx.db.sectionAssignment.findMany({
         where: {
           staffId: userId,
-          section: {
-            // TODO: if active term ever out of order for some reason, results could be weird
-            termId: { not: termId },
-          },
         },
         include: {
-          section: true,
+          section: {
+            include: {
+              professor: {
+                select: {
+                  email: true,
+                  name: true,
+                },
+              },
+              term: {
+                select: {
+                  termLetter: true,
+                  year: true,
+                },
+              },
+            },
+          },
         },
       });
 
@@ -92,13 +147,25 @@ export const studentDashboardRoute = createTRPCRouter({
     .query(async ({ input: { userId, termId }, ctx }) => {
       //TODO add auth check
 
+      const activeTerm = await ctx.db.term.findUnique({
+        where: { id: termId },
+      });
+
+      if (!activeTerm?.published) {
+        return null;
+      }
+
       const row = await ctx.db.sectionAssignment.findFirst({
         where: {
           staffId: userId,
           section: { termId: termId },
         },
         include: {
-          section: true,
+          section: {
+            include: {
+              professor: true,
+            },
+          },
         },
       });
 
