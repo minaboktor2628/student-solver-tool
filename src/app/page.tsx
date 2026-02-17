c395c09f0a82b8c7e4d436cc0d83c2062f22313f
minaboktor2628 <mina26b8@gmail.com>
2026-02-17 17:52:03 -0500

    add redirect for coordinator on frontpage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { auth } from "@/server/auth";
 import ProfessorHomePageComponent from "@/components/professor/professor-dashboard/professor-homepage";
-import { isAssistant, isProfessor } from "@/lib/utils";
+import { isAssistant, isCoordinator, isProfessor } from "@/lib/utils";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
   const session = await auth();
@@ -11,6 +12,9 @@ export default async function Home() {
     return <ProfessorHomePageComponent userId={session?.user?.id} />;
   } else if (isAssistant(session)) {
     return "hi assistant" + session.user.name;
+  } else if (isCoordinator(session) && !isProfessor(session)) {
+    // rare case, for testing
+    redirect("dashboard");
   } else {
     return "you dont belong here.";
   }
