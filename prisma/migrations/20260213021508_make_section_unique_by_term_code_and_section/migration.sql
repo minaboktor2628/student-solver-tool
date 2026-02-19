8c5337e94b5ea511bbbb72afd4a3b2b159a57691
minaboktor2628 <mina26b8@gmail.com>
2026-02-19 17:21:42 -0500

    flatten migrations

--- prisma/migrations/20260213021508_make_section_unique_by_term_code_and_section/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[termId,courseCode,courseSection]` on the table `Section` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Section_termId_courseCode_courseSection_key" ON "Section"("termId", "courseCode", "courseSection");
