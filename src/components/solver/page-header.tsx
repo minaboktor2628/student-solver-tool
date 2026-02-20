37cfd8a439dceff21fe9b3b4db8f6df5b0523a43
Jacob Burns <jacobaburns54@gmail.com>
2026-02-20 09:58:12 -0500

    Solver (#139)
    * added plumbing to accomidate solver
    
    * added better filtering to query
    
    * add on error toast
    
    * added solver strategy ui
    
    * Fix seed
    
    * added corejs to builtdependencies
    
    * add CtxType
    
    * add solver function maps
    
    * added check for isAvailableThisTerm to the solver data
    
    * Helpers for backtracking search
    
    * bts v1 working ?
    
    * Backtracking v1
    
    * Create seed2.ts
    
    * seed fix
    
    * Create deploy.sh
    
    * Update form-entry-preferences.tsx
    
    * Update .gitignore
    
    added local-seed to allow for per-branch test cases without changing seed in dev
    
    * fix lint errors
    
    * Delete deploy.sh
    
    ---------
    
    Co-authored-by: minaboktor2628 <mina26b8@gmail.com>

---
+++ src/components/solver/page-header.tsx
@@ -0,0 +1,107 @@
+"use client";
+import { ChevronDownIcon, InfoIcon } from "lucide-react";
+import { TermCombobox } from "../term-combobox";
+import { Button } from "../ui/button";
+import { ButtonGroup, ButtonGroupSeparator } from "../ui/button-group";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuLabel,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import Link from "next/link";
+import { api } from "@/trpc/react";
+import { toast } from "sonner";
+import { useState } from "react";
+import { solverStrategyMap, type SolverStrategy } from "@/lib/solver";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+export type SolverPageHeaderProps = {
+  termId: string;
+};
+
+export function SolverPageHeader({ termId }: SolverPageHeaderProps) {
+  const [solverStrategy, setSolverStrategy] =
+    useState<SolverStrategy>("greedy");
+
+  const utils = api.useUtils();
+
+  const solverApi = api.assignment.solve.useMutation({
+    onSuccess: () => {
+      toast.success("Successfully solved!");
+    },
+    onError: (err) => {
+      toast.error(err.message);
+    },
+    onSettled: async () => {
+      await Promise.all([
+        utils.courses.getAllCoursesForTerm.invalidate({ termId }),
+        utils.staff.getStaffForSection.invalidate(),
+      ]);
+    },
+  });
+
+  function handleSolve() {
+    solverApi.mutate({ termId, solverStrategy });
+  }
+
+  return (
+    <div className="flex items-center px-4">
+      <h1 className="pr-2 font-bold">Term: </h1> <TermCombobox />
+      <ButtonGroup className="ml-auto">
+        <ButtonGroup>
+          <Button
+            size="sm"
+            onClick={handleSolve}
+            disabled={solverApi.isPending}
+          >
+            Solve ({solverStrategyMap[solverStrategy].label})
+          </Button>
+          <ButtonGroupSeparator />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button size="sm">
+                <ChevronDownIcon />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end" className="w-44">
+              <DropdownMenuGroup>
+                <DropdownMenuLabel>Panel Position</DropdownMenuLabel>
+                <DropdownMenuRadioGroup
+                  value={solverStrategy}
+                  onValueChange={(value) =>
+                    setSolverStrategy(value as SolverStrategy)
+                  }
+                >
+                  {Object.entries(solverStrategyMap).map(([key, val]) => (
+                    <DropdownMenuRadioItem key={key} value={key}>
+                      {val.label}
+                      <Tooltip>
+                        <TooltipTrigger className="ml-auto">
+                          <InfoIcon />
+                        </TooltipTrigger>
+                        <TooltipContent>{val.description}</TooltipContent>
+                      </Tooltip>
+                    </DropdownMenuRadioItem>
+                  ))}
+                </DropdownMenuRadioGroup>
+              </DropdownMenuGroup>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </ButtonGroup>
+        <ButtonGroup>
+          <Button size="sm" asChild>
+            <Link href="/dashboard">Done</Link>
+          </Button>
+        </ButtonGroup>
+      </ButtonGroup>
+    </div>
+  );
+}
