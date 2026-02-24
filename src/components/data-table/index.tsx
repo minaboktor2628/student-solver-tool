fe0018d9f3ca154f0bab42ec759006b75b29d254
minaboktor2628 <mina26b8@gmail.com>
2026-02-23 22:15:10 -0500

    remove default cols

--- src/components/data-table/index.tsx
+++ src/components/data-table/index.tsx
@@ -1,5 +1,4 @@
 import { DataTable } from "./data-table";
 import { DataTableColumnHeader } from "./data-table-column-header";
-import { createDefaultColumns } from "./data-table-default-columns";
 
-export { DataTable, DataTableColumnHeader, createDefaultColumns };
+export { DataTable, DataTableColumnHeader };
