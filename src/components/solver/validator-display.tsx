d82c7c9f01544be8f319adb36068aef4596ff28a
minaboktor2628 <mina26b8@gmail.com>
2026-03-03 15:49:53 -0500

    counts in validator sheet

--- src/components/solver/validator-display.tsx
+++ src/components/solver/validator-display.tsx
@@ -1,6 +1,15 @@
 "use client";
 
 import { api } from "@/trpc/react";
+import {
+  Item,
+  ItemActions,
+  ItemContent,
+  ItemDescription,
+  ItemMedia,
+  ItemTitle,
+} from "@/components/ui/item";
+import { CheckIcon, InfoIcon } from "lucide-react";
 
 type ValidatorDisplayProps = { termId: string };
 
@@ -15,15 +24,87 @@ export function ValidatorDisplay({ termId }: ValidatorDisplayProps) {
 function ValidatorStaffGotPreferences(props: ValidatorDisplayProps) {
   const [{ userPrefRows, counts }] =
     api.validator.staffGotPreferences.useSuspenseQuery(props);
+
   return (
-    <code>
-      <pre>
-        {JSON.stringify(
-          userPrefRows.filter((d) => d.result.status !== "GOT_PREFERENCE"),
-          null,
-          2,
-        )}
-      </pre>
-    </code>
+    <div className="flex flex-col space-y-2">
+      <Item variant="outline">
+        <ItemMedia variant="icon">
+          {counts.UNASSIGNED === 0 ? (
+            <CheckIcon className="text-success" />
+          ) : (
+            <InfoIcon className="text-warning" />
+          )}
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>Unassigned Staff</ItemTitle>
+          <ItemDescription>Count: {counts.UNASSIGNED}</ItemDescription>
+        </ItemContent>
+      </Item>
+      <Item variant="outline">
+        <ItemMedia variant="icon">
+          {counts.ASSIGNED_NO_PREFS === 0 ? (
+            <CheckIcon className="text-success" />
+          ) : (
+            <InfoIcon className="text-warning" />
+          )}
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>Assigned staff w/o preferences</ItemTitle>
+          <ItemDescription>Count: {counts.ASSIGNED_NO_PREFS}</ItemDescription>
+        </ItemContent>
+      </Item>
+      <Item variant="outline">
+        <ItemMedia variant="icon">
+          {counts.ASSIGNED_NO_PREFS === 0 ? (
+            <CheckIcon className="text-success" />
+          ) : (
+            <InfoIcon className="text-warning" />
+          )}
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>
+            Assigned staff that didn&apos;t get their preferences
+          </ItemTitle>
+          <ItemDescription>
+            Count: {counts.ASSIGNED_NON_PREFERRED}
+          </ItemDescription>
+        </ItemContent>
+      </Item>
+      <Item variant="outline">
+        <ItemMedia variant="icon">
+          {counts.ASSIGNED_NO_PREFS === 0 ? (
+            <InfoIcon className="text-warning" />
+          ) : (
+            <CheckIcon className="text-success" />
+          )}
+        </ItemMedia>
+        <ItemContent>
+          <ItemTitle>Assigned staff that got their preferences</ItemTitle>
+          <ItemDescription className="flex flex-col">
+            <p>Count: {counts.GOT_PREFERENCE}</p>
+            <p>
+              Strong preference:{" "}
+              {
+                userPrefRows.filter(
+                  (u) =>
+                    u.result.status === "GOT_PREFERENCE" &&
+                    u.result.level === "STRONGLY_PREFER",
+                ).length
+              }
+            </p>
+            <p>
+              Preference:{" "}
+              {
+                userPrefRows.filter(
+                  (u) =>
+                    u.result.status === "GOT_PREFERENCE" &&
+                    u.result.level === "PREFER",
+                ).length
+              }
+            </p>
+          </ItemDescription>
+        </ItemContent>
+      </Item>
+    </div>
   );
 }
